7842d437d434183ef7fba9d2f53c5028cb365681
Afif Mohamed <afifmoh6923@gmail.com>
2025-08-01 10:51:41 -0500

    clock divider first

---
+++ src/clock_divider.sv
@@ -0,0 +1,16 @@
+module clock_divider (parameter DIVIDE_BY = 4)(
+    input logic fast_clock;
+    output logic slow_clock;
+)
+
+logic [$clog2(DIVIDE_BY) - 1:0] counter = 0;
+
+always @(posedge fast_clock) begin
+    if(counter == DIVIDE_BY - 1) begin
+        counter <= 0;
+    end else begin
+        counter <= counter + 1;
+    end
+end
+
+assign slow_clock = (counter < (DIVIDE_BY / 2)) ? 1 : 0;
