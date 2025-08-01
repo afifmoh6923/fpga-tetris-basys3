397bfe9672ae1118939cb2927f82b303a2dbbcff
Afif Mohamed <afifmoh6923@gmail.com>
2025-08-01 11:05:06 -0500

    debounce first vers completed

---
+++ src/debounce.sv
@@ -0,0 +1,13 @@
+module debounce (
+    input wire clk;
+    input wire noisy;
+    output logic clean;
+)
+
+logic [2:0] shift_reg;
+
+always @(posedge clk) begin
+    shift_reg <= {shift_reg[1:0], noisy};
+end
+
+assign clean = (shift_reg == 3'b111) ? 1 : 0;
